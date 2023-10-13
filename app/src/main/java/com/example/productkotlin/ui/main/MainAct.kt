b9f9b1df8ab379200a2a5c390e8bfed0815fa984
wjiacheng <wangjiacheng@angogo.cn>
2023-10-13 15:52:54 +0800

    添加并行协程示例

--- app/src/main/java/com/example/productkotlin/ui/main/MainAct.kt
+++ app/src/main/java/com/example/productkotlin/ui/main/MainAct.kt
@@ -1,6 +1,7 @@
 package com.example.productkotlin.ui.main
 
 import android.os.Bundle
+import androidx.lifecycle.pLifeScope
 import com.drake.serialize.intent.bundle
 import com.example.productkotlin.base.BaseVMAct
 import com.example.productkotlin.databinding.MainActBinding
@@ -8,6 +9,9 @@ import com.example.productkotlin.ui.activity.TestRVContract
 import com.jiaqiao.product.ext.*
 import com.jiaqiao.product.util.ProductThreadPool
 import com.jiaqiao.product.util.ProductUtil
+import kotlinx.coroutines.async
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.runBlocking
 
 
 class MainAct : BaseVMAct<MainActBinding, MainVM>() {
@@ -31,20 +35,29 @@ class MainAct : BaseVMAct<MainActBinding, MainVM>() {
         mViewBind.btn2.click { }
 
 
-//        launchIo {
+//        pLifeScope.launchIo {
 //
-//            val list = mutableListOf<Runnable>()
-//            for (i in 0..10) {
-//                list.add {
-//                    i.plog("posi")
-//                    Thread.sleep(1000 * 3)
-//                }
+//            "start".plog()
+//            val run1 = async {
+//                "run1".plog()
+//                delay(1000)
+//                "run1.end".plog()
 //            }
-//            "start pool".plog()
-//            ProductThreadPool.runAndWait(list)
-//            "end pool".plog()
+//            val run2 = async {
+//                "run2".plog()
+//                delay(2000)
+//                "run2.end".plog()
+//            }
+//            run1.await()
+//            run2.await()
+//            "end".plog()
 //
 //        }
+//        Thread{
+//            Thread.sleep(1200)
+//            pLifeScope.close()
+//            "close".plog()
+//        }.start()
 
     }
 
