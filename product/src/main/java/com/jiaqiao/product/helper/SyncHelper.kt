ecb8c5a4fe8f78bffe26ed9cb516677bd048978c
wjiacheng <wangjiacheng@angogo.cn>
2023-12-26 16:37:56 +0800

    添加协程并发示例

--- product/src/main/java/com/jiaqiao/product/helper/SyncHelper.kt
+++ product/src/main/java/com/jiaqiao/product/helper/SyncHelper.kt
@@ -1,6 +1,12 @@
 package com.jiaqiao.product.helper
 
+import androidx.core.content.ContentProviderCompat.requireContext
 import com.jiaqiao.product.ext.libPlog
+import com.jiaqiao.product.ext.notNull
+import com.jiaqiao.product.ext.scopeLaunchMain
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.async
+import kotlinx.coroutines.delay
 import kotlinx.coroutines.runBlocking
 import kotlinx.coroutines.suspendCancellableCoroutine
 import kotlinx.coroutines.withTimeout
@@ -16,11 +22,7 @@ object SyncHelper {
             runBlocking {
                 withTimeout(1000 * 2) {
                     suspendCancellableCoroutine { canCor ->
-                        var back = false
-                        if (!back) {
-                            back = true
-                            canCor.resume(1)
-                        }
+                        canCor.resume(1)
                     }
                 }
             }
@@ -28,6 +30,30 @@ object SyncHelper {
             thr.libPlog()
             null
         }
-
     }
+
+    /**
+     * 协程并发操作示例
+     */
+    private fun concurrent() {
+        scopeLaunchMain {
+
+            val run1 = this.async(Dispatchers.IO) {
+                delay(1000)
+            }
+            val run2 = this.async(Dispatchers.IO) {
+                delay(1000 * 2)
+            }
+            val run3 = this.async(Dispatchers.IO) {
+                delay(1000)
+            }
+
+            val result1 = run1.await()
+            val result2 = run2.await()
+            val result3 = run3.await()
+            //3个子线程协程执行完成，result为返回的结果
+
+        }
+    }
+
 }
